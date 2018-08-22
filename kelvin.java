33f130987e5f129369fd56a1b01ad3f528663fad
anusha-garre <41730901+anusha-garre@users.noreply.github.com>
2018-08-22 10:55:45 +0530

    Create kelvin.java

---
+++ kelvin.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int c,k;
+	  Scanner sc=new Scanner(System.in);
+	  c=sc.nextInt();
+	  k=c+273;
+	  System.out.println(k);// your code goes here
+	}
+}
