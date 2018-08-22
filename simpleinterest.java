fe133ef748e0d51b6288b2ae6d6f296d2d2a8a27
anusha-garre <41730901+anusha-garre@users.noreply.github.com>
2018-08-22 14:56:34 +0530

    Create simpleinterest.java

---
+++ simpleinterest.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int p,t,r,res=0;
+	  Scanner sc=new Scanner(System.in);
+	  p=sc.nextInt();
+	  t=sc.nextInt();
+	  r=sc.nextInt();
+      res=(p*t*r)/100;
+      System.out.println(res);
+	}
+}
