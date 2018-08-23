9ce4d0b62933c991b54d114b2e7822d3ad115928
anusha-garre <41730901+anusha-garre@users.noreply.github.com>
2018-08-23 15:32:59 +0530

    Create sqrt.java

---
+++ sqrt.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int b;
+        Scanner s=new Scanner(System.in);
+       b=s.nextInt();
+      int c=s.nextInt();
+      int h=b*c;
+      double j=Math.sqrt(h);
+      if(j==c){
+          System.out.println("yes");
+      }
+ else{
+          System.out.println("no");
+ }	// your code goes here
+	}
+}
